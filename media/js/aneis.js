f77b600faeea146457c5c0ec38be986568018a7c
Dark Creator <silvio_lucena_junior@hotmail.com>
2010-10-07 13:58:00 -0300

    Demo jogavel com o primeiro anel totalmente funcional. Adicionado pontos de vida e calculo do bonus de dano

--- media/js/aneis.js
+++ media/js/aneis.js
@@ -14,12 +14,20 @@ function AnelDaForcaDoMetal(){
     Anel.apply(this);
     
     this.tipo = OFENSIVO;
-    this.cargas = 5;
+    this.cargas = 1;
     
     this.usar = function(jogador){
         if (jogador.mao.forma_atual == TESOURA) {
             jogador.bonus_ataque = 1;
-            this.carga -= 1;
+            this.cargas -= 1;
+        }
+        if (this.cargas <= 0){
+            for (var i = 0; i < jogador.aneis_equipados.length; i++){
+                if (jogador.aneis_equipados[i] == this){
+                    jogador.aneis_equipados.splice(i,1);
+                }
+            }
+            alert("Anel Destruido");
         }
     }
 }
--- media/js/jogador.js
+++ media/js/jogador.js
@@ -1,17 +1,14 @@
 function Jogador(){
+    this.pontos_de_vida = 10;
+    this.pontos_de_vida_maximos = 10;
     this.nome = "";
     this.mao = null;
     this.bonus_ataque = 0;
-    
     this.aneis = [];
+    this.aneis_equipados = [];
     
     this.usar_anel = function(anel){
         anel.usar(this);
     }
-    
-    this.aneis_equipados = function(){
-        return this.aneis;
-    }
-    
-    
+        
 }
