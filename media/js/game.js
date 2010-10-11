2efb55a0176dd6791f24ccbf94eab8381070c4dc
Dark Creator <silvio_lucena_junior@hotmail.com>
2010-10-11 17:59:37 -0300

    Desenvolvimento do novo sistema do jogo retirando a logica solta da pagina index.html

--- media/js/game.js
+++ media/js/game.js
@@ -9,5 +9,38 @@ function init(){
 }
 
 function step(){
-    var vencedor = verificar_vitoria(jogador_1, jogador_2);
+    var resultado = verificar_vitoria(jogador_1, jogador_2);
+    if (resultado == 1){
+        vencedor = jogador_1;
+        perdedor = jogador_2;
+    } else if (resultado == -1){
+        vencedor = jogador_2;
+        perdedor = jogador_1;
+    }
+    
+    if (resultado != 0 ){
+        for (var i = 0; i < vencedor.aneis_equipados.length; i++){
+            var anel_equipado = vencedor.aneis_equipados[i];
+            if (anel_equipado.tipo == OFENSIVO){
+                vencedor.usar_anel(anel_equipado);
+            }   
+       }
+       for (var i = 0; i < perdedor.aneis_equipados.length; i++){
+            var anel_equipado = perdedor.aneis_equipados[i];
+            if (anel_equipado.tipo == DEFENSIVO){
+                perdedor.usar_anel(anel_equipado);
+            }   
+       }
+       var dano = vencedor.mao.pegar_dano_infligido(perdedor.mao) + vencedor.bonus_ataque + vencedor.bonus_base_ataque;
+       perdedor.pontos_de_vida -= dano - (perdedor.bonus_defesa + perdedor.bonus_base_defesa);
+       if (vencedor.bracelete_equipado != null){
+           vencedor.usar_bracelete(vencedor.bracelete_equipado, perdedor);
+       }    
+       vencedor.bonus_ataque = 0;
+       vencedor.bonus_defesa = 0;
+       perdedor.bonus_ataque = 0;
+       perdedor.bonus_defesa = 0;
+       alert("O Jogador 1 possui " + jogador_1.pontos_de_vida + " pontos de vida");
+       alert("O Jogador 2 possui " + jogador_2.pontos_de_vida + " pontos de vida");
+    }
 }
--- media/js/jogador.js
+++ media/js/jogador.js
@@ -11,12 +11,17 @@ function Jogador(){
     this.aneis_equipados = [];
     this.obeliscos = [];
     this.obeliscos_equipados = [];
+    this.bracelete_equipado = null;
     
     this.usar_anel = function(anel){
         anel.usar(this);
-    }
+    };
+    
+    this.usar_bracelete = function(bracelete, oponente){
+        bracelete.calcular_bonus(this, oponente);
+    };
     
     this.usar_obelisco = function(obelisco, oponente){
         obelisco.ativar_efeito(this, oponente)        
-    }
+    };
 }
